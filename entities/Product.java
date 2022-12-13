3dd99ac0bd26281a824664ba156b5b663d25b37e
IzaquielBernadi <izaquielbernadino@gmail.com>
2022-12-13 14:47:12 -0300

    Final project

---
+++ entities/Product.java
@@ -0,0 +1,41 @@
+package entities;
+
+public class Product {
+    private String name;
+    private Integer quantity;
+    private Double value;
+
+    public Product(String name, Integer quantity, Double value) {
+        this.name = name;
+        this.quantity = quantity;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public Double total() {
+        return value * quantity;
+    }
+}
